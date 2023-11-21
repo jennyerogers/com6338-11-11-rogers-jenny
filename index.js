5ff276493895bd7bf3929f37112533b715e36b8d
Jenny Rogers <jrogers1@ufl.edu>
2023-11-21 10:23:29 -0500

    fix

--- index.js
+++ index.js
@@ -10,17 +10,20 @@ const makeTag = tag => str => `<${tag}>${str}</${tag}>`
 
 // start ftn to make poem html
 const makePoemHTML = (poemData) => { //retrieve poem data
+
   const {0: { author, lines, title } } = poemData
   console.log(poemData)
   //should return HTML string that contains title in h2, then author in em in h3, and then paragraph tags
   const poemTitle = makeTag('h2')(title) 
   const authorTitle = pipe(makeTag('em'), makeTag('h3'))(`by ${author}`)
   //set stanzas
-  const poemLine = lines.join('<br>').split('<br><br>') //should place <br> between each line of text inside the paragraph tag
+  const poemLine = lines.join("<br>").split("<br><br>") //should place <br> between each line of text inside the paragraph tag
   const poemStanza = poemLine.map(stanza => makeTag('p')(stanza.split('<br>').join('<br>')))
+
   return `${poemTitle}${authorTitle}${poemStanza.join('')}` //display
 }
 
 getPoemBtn.onclick = async function() { //Clicking "Get Poem" should render poem HTML to #poem
+
   poemEl.innerHTML = makePoemHTML(await getJSON(poemURL))
 }
